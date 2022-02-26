0fce4c9a351a688f3c8d189281e853df670254da
zmb6893 <zmb6893@rit.edu>
2022-02-26 18:56:37 -0500

    Added the studyLocation enumeration

---
+++ tiger-api/src/main/java/com/tigermeet/api/tigermeetapi/model/StudyLocation.java
@@ -0,0 +1,31 @@
+package com.tigermeet.api.tigermeetapi.model;
+
+/**
+ * The class representation for a study location.
+ * @author zmb6893
+ */
+
+public enum StudyLocation {
+    FIRESIDE_LOUNGE("Fireside Lounge", "68 Lomb Memorial Dr, Rochester, NY 14623");
+
+    private String location;
+    private String studyArea;
+
+    private StudyLocation(String studyArea, String location){
+        this.studyArea = studyArea;
+        this.location = location;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public String getStudyArea(){
+        return this.studyArea;
+    }
+
+    @Override
+    public String toString() {
+        return this.studyArea + " at " + this.location;
+    }
+}
